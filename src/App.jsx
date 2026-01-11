26ac13b9d3b5056764f3ca2bb6599f1f48bd6f81
Roger Thomas <rogerthomas96@outlook.com>
2026-01-10 21:51:56 -0600

    added core concept component, data.js file for that component, and displayed that component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,12 +22,46 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description} 
+              img={CORE_CONCEPTS[0].image} 
+            />
+            <CoreConcept 
+              title={CORE_CONCEPTS[1].title} 
+              description={CORE_CONCEPTS[1].description} 
+              img={CORE_CONCEPTS[1].image} 
+            />
+            <CoreConcept 
+            title={CORE_CONCEPTS[2].title} 
+            description={CORE_CONCEPTS[2].description} 
+            img={CORE_CONCEPTS[2].image} 
+            />
+            <CoreConcept 
+            title={CORE_CONCEPTS[3].title} 
+            description={CORE_CONCEPTS[3].description} 
+            img={CORE_CONCEPTS[3].image} 
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
