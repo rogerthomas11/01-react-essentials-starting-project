fff0e27548b6c95a7c0012a542af80e662ed6600
Roger Thomas <rogerthomas96@outlook.com>
2026-01-15 15:58:40 -0600

    added funtional tabs to display info on core concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+    function handleSelect(selectedButton) {
+        console.log(selectedButton);
+    }
+
   return (
     <div>
       <Header />
@@ -15,6 +20,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+
         </section>
       </main>
     </div>
