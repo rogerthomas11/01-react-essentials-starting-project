5eae1810fe4ccee829caf204aabbf71cede9055f
Roger Thomas <rogerthomas96@outlook.com>
2026-01-20 17:44:15 -0600

    added examples for tabs in data file and added code to dynamically select them to display inside app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState();
+
     function handleSelect(selectedButton) {
-        console.log(selectedButton);
+      setSelectedTopic(selectedButton);
+        // console.log(selectedTopic);
     }
 
   return (
@@ -29,7 +35,19 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-
+          {!selectedTopic ? (
+            <p>Please select a topic</p>
+          ) : (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+          )}
         </section>
       </main>
     </div>
